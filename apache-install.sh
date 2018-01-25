7734182185c26a446ef982669f9610e72586f0c5
tas7154 <theaquarianspace@gmail.com>
2018-01-25 23:07:52 +0000

    Created new index.html

--- apache-install.sh
+++ apache-install.sh
@@ -4,3 +4,9 @@ sudo systemctl start httpd        # start apache
 sudo systemctl enable httpd       # enable apache on start    
 
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf     # Comment contents of welcome.conf
+
+echo -e "
+<html>\n
+<h1>Welcome NTI 300</h1>\n
+</html>
+" > /var/www/html/index.html
