6836203bcb2658652d129949a9e9da30f6e5b37a
tas7154 <theaquarianspace@gmail.com>
2018-01-25 23:01:21 +0000

    added line to comment out welcome.conf

--- apache-install.sh
+++ apache-install.sh
@@ -3,4 +3,4 @@ sudo yum install httpd mod_ssl -y # Install apache and mod_ssl
 sudo systemctl start httpd        # start apache        
 sudo systemctl enable httpd       # enable apache on start    
 
-
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf     # Comment contents of welcome.conf
